e69c459e106a1f8e0ea81c56c7935b18fb73958d
Maulana Zahiri <147925317+yolookings@users.noreply.github.com>
2025-04-08 14:46:14 +0700

    Initial Commit

---
+++ daily-memos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  daily-memos
+//
+//  Created by Maulana Ahmad Zahiri on 08/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ daily-memosTests/daily_memosTests.swift
@@ -0,0 +1,17 @@
+//
+//  daily_memosTests.swift
+//  daily-memosTests
+//
+//  Created by Maulana Ahmad Zahiri on 08/04/25.
+//
+
+import Testing
+@testable import daily_memos
+
+struct daily_memosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
